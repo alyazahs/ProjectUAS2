8f2d0d64a4021e7d546309d0f5c8f20d50e66b34
LAPTOP-G1UJF14B\user <alyak536@gmail.com>
2024-05-28 09:49:33 +0700

    login

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+$server = "localhost";
+$user = "root";
+$password = "kopi";
+$nama_database = "we_shine";
+$db = mysqli_connect($server, $user, $password, $nama_database);
+if( !$db ){
+ die("Gagal terhubung dengan database: " . mysqli_connect_error());
+}
+?>
