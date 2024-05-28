8bb3ffcde02baf9162e4e462ed6ecdb86ca1b075
Shalfa Amelia <shalfaamelia@gmail.com>
2024-05-28 09:52:04 +0700

    ppp

--- config.php
+++ config.php
@@ -1,10 +1,14 @@
 <?php
+
 $server = "localhost";
 $user = "root";
-$password = "kopi";
+$password = "";
 $nama_database = "we_shine";
+
 $db = mysqli_connect($server, $user, $password, $nama_database);
+
 if( !$db ){
- die("Gagal terhubung dengan database: " . mysqli_connect_error());
+    die("Gagal terhubung dengan database: " . mysqli_connect_error());
 }
+
 ?>
