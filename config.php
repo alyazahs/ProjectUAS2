eea107964994b9d4f389fd1e06d496824248bf9a
AlyaZahra <alyazahs13@gmail.com>
2024-05-28 19:12:00 +0700

    UHUYYY

--- config.php
+++ config.php
@@ -1,20 +1,14 @@
 <?php
-
-$server = "localhost";
-$user = "root";
+$servername = "localhost";
+$username = "root";
 $password = "";
-<<<<<<< HEAD
 $dbname = "we_shine";
-=======
-$nama_database = "we_shine";
->>>>>>> 5c705008c0c486d664dbe5c5f9bf412d0901b110
 
-// Membuat koneksi
-$conn = mysqli_connect($server, $user, $password, $nama_database);
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
 
-// Memeriksa koneksi
-if (!$conn) {
-    die("Gagal terhubung dengan database: " . mysqli_connect_error());
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
 }
-
 ?>
