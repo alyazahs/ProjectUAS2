69372506a018628f6e20c1fafa94649ffb2b4a7f
AlyaZahra <alyazahs13@gmail.com>
2024-05-29 10:51:12 +0700

    BARUU REKK

---
+++ navbar.php
@@ -0,0 +1,57 @@
+<nav class="navbar navbar-expand-lg navbar-light" style="background-image: url('img/bottle.png');">
+    <a class="navbar-brand" href="#">
+        <img src="img/logo.png" class="align-center rounded-circle" style="height: 90px; width: 90px;">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link text-white" href="index.php">HOME</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdownCategory" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    CATEGORY
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownCategory">
+                    <a class="dropdown-item" href="category1.php">MAKE UP</a>
+                    <a class="dropdown-item" href="category2.php">SKINCARE</a>
+                    <a class="dropdown-item" href="category3.php">BODYCARE</a>
+                    <a class="dropdown-item" href="category4.php">HAIRCARE</a>
+                </div>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdownRecomendation" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    RECOMMENDATION
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownRecomendation">
+                    <a class="dropdown-item" href="recommendation1.php">PRODUCT SKINCARE</a>
+                    <a class="dropdown-item" href="recommendation2.php">PRODUCT MAKE UP</a>
+                    <a class="dropdown-item" href="recommendation3.php">PRODUCT BODYCARE</a>
+                    <a class="dropdown-item" href="recommendation4.php">PRODUCT HAIRCARE</a>
+                </div>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdownGRWU" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    GRWU
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownGRWU">
+                    <a class="dropdown-item" href="grwu1.php">SKINCARE ROUTINE</a>
+                    <a class="dropdown-item" href="grwu2.php">MAKE UP TUTORIAL</a>
+                    <a class="dropdown-item" href="grwu3.php">FASHION</a>
+                </div>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle text-white" href="#" id="navbarDropdownJoinUs" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    JOIN US
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownJoinUs">
+                    <a class="dropdown-item" href="signup.php">SIGN UP</a>
+                    <a class="dropdown-item" href="profile.php">PROFILE</a>
+                </div>
+            </li>
+        </ul>
+    </div>
+</nav>
