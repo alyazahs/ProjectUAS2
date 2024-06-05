f1e569bd297cbe6bcc5c5ec587d5d11271d50909
AlyaZahra <alyazahs13@gmail.com>
2024-06-05 08:01:44 +0700

    fnl

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php include 'navbar.php'; ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,42 +7,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>WE SHINE!</title>
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="styles.css">
     <style>
-      body {
-            margin: 0;
-      }
         .bg-section {
             background-image: url('img/bg2.png');
-        }
-        .category-item {
-            display: flex;
-            flex-direction: column;
-            text-align: center;
-            position: relative;
-        }
-        .category-item img {
-            width: 120px;
-            height: 120px;
-            border-radius: 60px;
-            transition: transform 0.3s ease;
-        }
-        .category-item span {
-            margin-top: 10px;
-        }
-        .category-item:hover img {
-            transform: scale(1.1);
-        }
-        .navbar {
-            position: fixed; /* Tetap pada posisi saat discroll */
-            width: 100%; /* Menempati lebar penuh */
-            z-index: 1000;
-            padding-top: 0; /* Atur padding atas navbar menjadi 0 */
-            margin-top: 0;
         }
     </style>
 </head>
 <body class="bg-light">
-<?php include 'navbar.php'; ?>
     <div class="AboutUs">
         <img src="img/skincare1.png" alt="" style="position: absolute; z-index: 1; object-fit: cover; width: 100%; height: 100%; left: 0; top: 115px;">
         <section class="text-dark font-italic" style="margin-left: 300px; margin-right: 300px; margin-top: 30px; text-align: center; padding-left: 30px; padding-right: 30px; padding-top: 100px; padding-bottom: 30px; border-radius: 30px; position: relative; z-index: 2;">
@@ -51,24 +25,24 @@
 
     <h1 class="text-dark font-italic text-center" style="margin-top:250px;">All You Need</h1>
     <div class="category-container" style="margin-bottom: 100px; margin-top: 100px; align-items: flex-start; display: flex; justify-content: space-around; text-align: center;">
-        <div class="category-item">
-            <img src="img/skincare2.jpg">
+        <div class="category-item" style="display: flex; flex-direction: column; text-align: center;">
+            <img src="img/skincare2.jpg" style="width: 120px; height: 120px; border-radius: 60px;">
             <span>SKINCARE</span>
         </div>
-        <div class="category-item">
-            <img src="img/makeup.jpg">
+        <div class="category-item" style="display: flex; flex-direction: column; text-align: center;">
+            <img src="img/makeup.jpg" style="width: 120px; height: 120px; border-radius: 60px;">
             <span>MAKE UP</span>
         </div>
-        <div class="category-item">
-            <img src="img/soap.jpg">
+        <div class="category-item" style="display: flex; flex-direction: column; text-align: center;">
+            <img src="img/soap.jpg" style="width: 120px; height: 120px; border-radius: 60px;">
             <span>BODYCARE</span>
         </div>
-        <div class="category-item">
-            <img src="img/haircare.jpg">
+        <div class="category-item" style="display: flex; flex-direction: column; text-align: center;">
+            <img src="img/haircare.jpg" style="width: 120px; height: 120px; border-radius: 60px;">
             <span>HAIRCARE</span>
         </div>
-        <div class="category-item">
-            <img src="img/clotch.jpg">
+        <div class="category-item" style="display: flex; flex-direction: column; text-align: center;">
+            <img src="img/clotch.jpg" style="width: 120px; height: 120px; border-radius: 60px;">
             <span>FASHION</span>
         </div>
     </div>
@@ -83,14 +57,14 @@
 
     <div class="class" style="display: flex;">
         <div class="contact mx-5">
-            <h5 style="flex: 1; margin-top: 110px; font-weight: bold;">CONTACT US</h5>
-                <a class="class text-dark" style="display: flex; margin-bottom: 10px;">Jl. Nomad No. 13, Klegen, Kec. Kartoharjo, Kota Madiun, Jawa Timur 63117</a>
-                <a class="class text-dark" href="mailto:weshine.ofc@gmail.com" style="display: flex; margin-bottom: 10px;">weshine.ofc@gmail.com</a>
-                <a class="class text-dark" style="display: flex; margin-bottom: 10px;">Contact (+62)83177768080</a>
+            <h5 style="flex: 0; margin-top: 110px; font-weight: bold;">CONTACT US</h5>
+            <p>Jl. Nomad No. 13, Klegen, Kec. Kartoharjo, Kota Madiun, Jawa Timur 63117</p>
+            <p>weshine.ofc@gmail.com</p>
+            <p>Contact (+62)83177768080</p>
         </div>
 
         <div class="sosmed mx-5">
-            <h5 style="flex: 1; margin-top: 110px; font-weight: bold;">SOCIAL MEDIA</h5>
+            <h5 style="flex: 1; margin-top: 110px; font-weight: bold;">SOSIAL MEDIA</h5>
             <a class="class text-dark" href="https://www.instagram.com/we_shine.ofc?igsh=YzljYTk1ODg3Zg==" style="display: flex; margin-bottom: 10px;">
                 <img src="img/instagram.jpg" style="height: 30px; width: 30px; border-radius: 5px; margin-right: 10px;">@we_shine.ofc
             </a>
